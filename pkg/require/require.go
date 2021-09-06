7947cf7d83eede8cf4dc8fc887e734d1a7142e99
Marc-Antoine Argenton <maargenton.dev@gmail.com>
2021-09-06 13:14:43 -0700

    Add optional context to verify.That() / require.That()

--- pkg/require/require.go
+++ pkg/require/require.go
@@ -7,14 +7,17 @@ import (
 	"github.com/maargenton/go-testpredicate/pkg/internal/predicate"
 )
 
+type Context = predicate.ContextValue
+
 // That captrues the test context and a value for the purpose of building and
 // evaluating a test predicate through call chaining. The current test will
 // fail immediately if the predicate fails.
-func That(t predicate.T, v interface{}) *builder.Builder {
+func That(t predicate.T, v interface{}, ctx ...Context) *builder.Builder {
 	var b = builder.New(t, v, true)
 	builder.CaptureCallsite(b, 1)
 	t.Cleanup(func() {
 		builder.VerifyCompletness(b)
 	})
+	b.Ctx = append(b.Ctx, ctx...)
 	return b
 }
